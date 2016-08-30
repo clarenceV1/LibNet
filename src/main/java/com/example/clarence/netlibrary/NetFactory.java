939141b3c1775f5f235315ab7448cbc681761818
caishuxing <caishuxing@linggan.com>
2016-08-30 08:44:09 +0800

    添加库

---
+++ src/main/java/com/example/clarence/netlibrary/NetFactory.java
@@ -0,0 +1,10 @@
+package com.example.clarence.netlibrary;
+
+/**
+ * 获取各种类型的网络请求入口
+ * Created by clarence on 16/6/20.
+ */
+public class NetFactory {
+
+
+}
---
+++ src/test/java/com/example/clarence/netlibrary/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.clarence.netlibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void testHttp() {
+        assertEquals(4, 2 + 2);
+    }
+}
